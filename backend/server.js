ef86125ab41dffc8a5fa2c05199a47e3da6f8987
Hamza Patel <patelhamza46@gmail.com>
2026-03-15 13:00:04 +0530

    added db and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-hello
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/mongodb.js";
+
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB();
+
+app.use(express.json());  
+app.use(cookieParser());
+
+app.get("/", (req,res) => {
+  res.send("backend working");
+});
+
+app.listen(port, () => { 
+  console.log(`Server started on PORT:${port}`) 
+});
