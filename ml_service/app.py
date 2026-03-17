6e1cd738aeba3228ccd62d2607ebe563401ab3d4
Mho <ommtripathi046@gmail.com>
2026-03-17 22:58:41 +0530

    ml backend working

--- ml_service/app.py
+++ ml_service/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
+import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -122,3 +123,4 @@ def home():
             "/predict/hate"
         ]
     }
+    
