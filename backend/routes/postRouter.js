b66a492f9f0717dec652b0cb88001885a08c34c2
Mho <ommtripathi046@gmail.com>
2026-03-18 19:01:28 +0530

    greivance backend done

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,17 +1,21 @@
 const express = require("express")
-
 const router = express.Router()
 
 const {auth} = require("../middleware/userMiddleware")
 
 const {createComment , getPostComments} = require("../controllers/commentController")
 const {createPost} = require("../controllers/postController")
+const {createGreivance} = require("../controllers/greivanceController")
 
+
+// Post related Controllers
+router.post("/post",auth,createPost)
 router.post("/create-comment", auth, createComment);
 router.get("/get-comments", getPostComments);
 
 
-router.post("/post",auth,createPost)
+// Grievance 
+router.post("/create-greivance",auth,createGreivance)
 
 
 module.exports = router
