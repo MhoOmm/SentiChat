2f1d75184b47331d658c68ff8903eebdf3236190
Mho <ommtripathi046@gmail.com>
2026-03-19 02:33:01 +0530

    grievance page added

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "../pages/AuthPage";
 import AdminLogin from "../pages/AdminLogin"
 import AdminDashboard from "../pages/AdminDashboard";
+import GrievancePage from "../pages/GrievancePage"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/auth" />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/greivance" element={<GrievancePage />} />
         <Route path="/admin/login-admin" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
